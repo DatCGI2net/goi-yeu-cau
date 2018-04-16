2c494c1e49e5b61706f73cc614aad5263488e25f
TTD <dat@cgito.net>
2018-04-16 21:24:35 +0700

    changed text in HomePage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -4,8 +4,8 @@ import {Link} from 'react-router';
 
 const HomePage = () => (
     <div className="jumbotron center">
-        <h1 className="lead">Chào mừng quí khách!</h1>
-        <div className="heading">Something fun will be show</div>
+        <h1 className="lead">Welcome to my small reactjs app with redux-saga and djang rest backend!</h1>
+        <div className="heading">It allows to login, post request and list/search requests</div>
         <div>
         <Link to="requests">
         <button className="btn bt-lg btn-primary">View Requests</button>
